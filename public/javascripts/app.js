f76850d686d28cfa6ac5df9ed95e428cfc5c0434
Conor Kingston <cmkingston21@me.com>
2016-09-14 16:48:08 -0600

    fixed toRoute bug

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -125,7 +125,7 @@ app
   ])
 
   .controller('jwtController',['$rootScope','$scope','$http','principal','authorization','$state',function($rootScope,$scope,$http,principal,authorization,$state) {
-    if($rootScope.returnToState && $rootScope.returnToState === $state.current.name) {
+    if($rootScope.returnToState && $rootScope.returnToState.name === $state.current.name) {
       delete $rootScope.returnToState;
       delete $rootScope.returnToStateParams;l
     }
